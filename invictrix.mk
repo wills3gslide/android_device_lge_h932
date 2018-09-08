c328b45da701b456568aae7be3c70c0dc1783c70
me2151 <me2151@invictrixrom.com>
2018-09-08 16:14:42 -0400

    Invictify

--- invictrix.mk
+++ invictrix.mk
@@ -1,12 +1,14 @@
 $(call inherit-product, device/lge/h930/full_h930.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/invictrix/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h930
+PRODUCT_NAME := invictrix_h930
+
+INVICTRIX_MAINTAINER := me2151
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
@@ -15,5 +17,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 1819221128e3d release-keys"
 
 BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/1819221128e3d:user/release-keys"
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,2 @@
-add_lunch_combo lineage_h930-userdebug
+add_lunch_combo invictrix_h930-userdebug
+add_lunch_combo invictrix_h930-eng
